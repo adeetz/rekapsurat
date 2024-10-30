3956d8375f9bb200a7e7a3086bfc5c43a7109539
adeetz <m.noor.aditya.rahman@gmail.com>
2024-10-30 09:37:28 +0800

    update auth

--- api/auth.php
+++ api/auth.php
@@ -1,9 +1,9 @@
 <?php
 // api/auth.php
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: *');
-header('Access-Control-Allow-Methods: *');
-header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");  // Atau domain spesifik
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
